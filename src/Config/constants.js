985a11f619b7acfb9af7068891235e3fd39b6111
Freddecarcans <frederic.tabbal@gmail.com>
2019-11-18 09:40:55 +0100

    Ménage

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,8 +1,8 @@
-// export const urlApi = "http://192.168.1.72:3000/api";
+export const urlApi = "http://192.168.1.72:3000/api";
 
 // export const urlApi = "http://192.168.43.57:3000/api";
 
-export const urlApi = "http://192.168.1.30:3000/api";
+// export const urlApi = "http://192.168.1.30:3000/api";
 
 
 export const temp = 'temp';
